b2445a4f7fd55cf0163d22744670ccf5e54da181
старченко дмитрий <starchenko.dmitr@mail.ru>
2025-11-30 18:58:05 +0700

    Добавлена страница просмотра информации о товаре, произведена настройка, дополнен README.md

--- catalog/views.py
+++ catalog/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import ContactInfo, Product
+from django.core.paginator import Paginator
 
 
 def home(request):
-    products = Product.objects.all()  # ← получаем все продукты
-    print("Products count:", products.count())  # ← временная отладка
+    products_list = Product.objects.all()
+    paginator = Paginator(products_list, 8)  # 8 товаров на страницу (можно изменить)
+
+    page_number = request.GET.get('page')
+    products = paginator.get_page(page_number)
+
     return render(request, 'catalog/home.html', {'products': products})
 
 
@@ -18,3 +23,9 @@ def contacts(request):
         print(message)
         return HttpResponse(f"Спасибо, {name}! Ваше сообщение получено.")
     return render(request, 'catalog/contacts.html', {'contact_info': contact_info})
+
+
+def product_detail(request, product_id):
+    product = Product.objects.get(id=product_id)
+    context = {"product": product}
+    return render(request, "catalog/product_detail.html", context)
