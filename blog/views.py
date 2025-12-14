5b82e81f9b8b49bf8aeaee0904fc05b2ed4dc4fd
старченко дмитрий <starchenko.dmitr@mail.ru>
2025-12-14 15:49:13 +0700

    контроллеры изменены с FBV на CBV, добавлен блог с реализован полный CRUD

---
+++ blog/views.py
@@ -0,0 +1,47 @@
+from django.urls import reverse, reverse_lazy
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
+from .models import BlogPost
+
+class BlogPostListView(ListView):
+    model = BlogPost
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        return BlogPost.objects.filter(is_published=True)
+
+class BlogPostDetailView(DetailView):
+    model = BlogPost
+    template_name = 'blog/post_detail.html'
+    context_object_name = 'post'
+
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset=queryset)
+        obj.views_count += 1
+        obj.save(update_fields=['views_count'])
+        return obj
+
+class BlogPostCreateView(CreateView):
+    model = BlogPost
+    template_name = 'blog/post_form.html'
+    fields = ['title', 'content', 'preview', 'is_published']
+    success_url = reverse_lazy('blog:post_list')
+
+class BlogPostUpdateView(UpdateView):
+    model = BlogPost
+    template_name = 'blog/post_form.html'
+    fields = ['title', 'content', 'preview', 'is_published']
+
+    def get_success_url(self):
+        return reverse('blog:post_detail', kwargs={'pk': self.object.pk})
+
+class BlogPostDeleteView(DeleteView):
+    model = BlogPost
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = reverse_lazy('blog:post_list')
--- config/urls.py
+++ config/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(('catalog.urls', 'catalog'), namespace='catalog')),
+    path('', include(('blog.urls', 'blog'), namespace='blog')),
 ]
 
 # Только при DEBUG = True!
